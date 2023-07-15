429063fe9e167842fc2d30057efc8834ef2f580e
Felipe Bomfim <felipebomfim4161@gmail.com>
2023-07-14 22:19:14 -0300

    Completed the challenge about selecting a random element in array

---
+++ Section14_IntermediateJavaScript/whoIsPaying.js
@@ -0,0 +1,13 @@
+function whosPaying(names) {
+
+  /******Don't change the code above*******/
+
+  //Write your code here.
+
+  let payerIndex = Math.floor(names.length * Math.random());
+  let payerName = names[payerIndex];
+  return names[payerIndex] + " is going to buy lunch today!"
+
+
+  /******Don't change the code below*******/
+}
