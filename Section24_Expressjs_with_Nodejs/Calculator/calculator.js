f2582f24afdee514906993e5437b9ab2f39f44b1
Felipe Bomfim <felipebomfim4161@gmail.com>
2023-07-16 23:18:55 -0300

    Updated the calculator to include BMI calculation.

--- Section24_Expressjs_with_Nodejs/Calculator/calculator.js
+++ Section24_Expressjs_with_Nodejs/Calculator/calculator.js
@@ -8,9 +8,20 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 })
 
+app.get("/bmiCalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
 app.post("/", (req, res) => {
   res.send(`The sum of ${req.body.num1} and ${req.body.num2} is: ${Number(req.body.num1) + Number(req.body.num2)}`);
   console.log(req.body);
+})
+
+app.post("/bmiCalculator", (req, res) => {
+  let height = Number(req.body.height);
+  let weight = Number(req.body.weight);
+  let bmi = weight / (height ** 2);
+  res.send(`Your BMI is <b>${bmi.toFixed(2)}</b>.`);
 })
 
 app.listen(8080, () => {
